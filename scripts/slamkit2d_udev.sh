733158f4ccf9f72e116b1d48cafc05a57a401afb
neo <neo@server>
2025-11-30 23:59:41 +0800

    modify lidar driver launch

--- scripts/slamkit2d_udev.sh
+++ scripts/slamkit2d_udev.sh
@@ -1,5 +1,5 @@
+echo 'KERNELS=="1-1.1", MODE:="0666", GROUP:="dialout", SYMLINK+="lidar"' >> /etc/udev/rules.d/slamkit2d.rules
 echo 'KERNELS=="1-1.3", MODE:="0666", GROUP:="dialout", SYMLINK+="imu"' >> /etc/udev/rules.d/slamkit2d.rules
-echo 'KERNELS=="1-1.2", MODE:="0666", GROUP:="dialout", SYMLINK+="lidar"' >> /etc/udev/rules.d/slamkit2d.rules
 
 # service udev reload
 # sleep 2
