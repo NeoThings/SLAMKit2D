780898e60694908423c6b7d428be1e1e22c3477c
neo <neo@server>
2025-12-01 22:49:42 +0800

    debug

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -1,3 +1,3 @@
-sudo cp ./bringup.desktop ${HOME}.local/share/applications/bringup.desktop
+cp ./bringup.desktop ${HOME}/.local/share/applications/bringup.desktop
 
 update-desktop-database ~/.local/share/applications
