2d60881546779718a295e60ebaa9b01446ea8fc9
Neo <neo@neobox>
2025-12-02 18:08:14 +0800

    update install launcher scripts

--- scripts/install_app.sh
+++
@@ -1,3 +0,0 @@
-cp ./bringup.desktop ${HOME}/.local/share/applications/bringup.desktop
-
-update-desktop-database ~/.local/share/applications
---
+++ scripts/install_launchers.bash
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cp ../asset/icons/radar.png ~/.local/share/icons/
+
+cp ../configs/slamkit2d_bringup.desktop ~/.local/share/applications/
+
+update-desktop-database ~/.local/share/applications
